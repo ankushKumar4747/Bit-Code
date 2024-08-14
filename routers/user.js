041e7548b008341f93659233bad9a774c5a4c401
ankushKumar4747 <155870580+ankushKumar4747@users.noreply.github.com>
2024-08-14 20:53:34 +0530

    register router added

--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,7 @@
 const router=require("express").Router();
 const Controller=require('../controller/index')
 
-router.get("/register",Controller.userController.register)
+router.post("/register",Controller.userController.register)
 
 module.exports=router;
 
