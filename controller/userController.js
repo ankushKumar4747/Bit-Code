51432b437de897364e789a407118f50f9669d9df
ankushKumar4747 <155870580+ankushKumar4747@users.noreply.github.com>
2024-08-14 10:14:51 +0530

    new commit

--- controller/userController.js
+++ controller/userController.js
@@ -3,6 +3,7 @@ const Validation=require("../Validations/index")
 module.exports.register=(req,res)=>{
     try{
         Validation.User.register.validateAsync(req.body);
+        console.log();
         
 
     }catch(error){
