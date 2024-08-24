0224c24a0f5e2e977a96fa81a66e502b6bde25f9
AnkushKumar <ankushbhagat4747@gmail.com>
2024-08-24 11:15:47 +0530

    messages updated

--- common/constants.js
+++ common/constants.js
@@ -1,6 +1,9 @@
 module.exports={
     MESSAGES:{
         EMAIL_ALREADY_EXIST:"email is already exist",
-        REGISTER_SUCCESSFULLY:"user register successfully"
+        REGISTER_SUCCESSFULLY:"user register successfully",
+        OTP_SEND_SUCCESSFULLY:"otp send successfully",
+        OTP_INVALID:"invelid otp or user not found",
+        OTP_VERIFY_SUCCESSFULLY:"otp verify successfully",  
     }
 }
