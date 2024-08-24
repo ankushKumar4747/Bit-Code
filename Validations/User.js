d564e51079551def35d150e8a8f9a47f48265e0a
AnkushKumar <ankushbhagat4747@gmail.com>
2024-08-24 11:20:05 +0530

    validations updated

--- Validations/User.js
+++ Validations/User.js
@@ -6,3 +6,8 @@ module.exports.register=joi.object({
     email:joi.string().required(),
     password:joi.string().required()
 })
+
+module.exports.otpVerify=joi.object({
+    otp:joi.string().required(),
+    _id:joi.string().required()
+})  
