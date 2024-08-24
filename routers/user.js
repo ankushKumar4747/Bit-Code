308b1a269a5e164453b8f39696aa4e51d5d9de03
AnkushKumar <ankushbhagat4747@gmail.com>
2024-08-24 11:17:56 +0530

    otpVerify handler add

--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,8 @@
 const router=require("express").Router();
 const Controller=require('../controller/index')
 
-router.post("/register",Controller.userController.register)
+router.post("/register",Controller.userController.register);
+router.post("/otpVerify",Controller.userController.otpVerify);
 
 module.exports=router;
 
